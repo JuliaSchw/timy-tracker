88ec612b712c12aef6a628037d43bf4cafbe5172
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-02-22 16:51:08 +0100

    add page structure for timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const TimerSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  gap: 20px;
+`;
 
 const HomePage: React.FC = () => {
+  const currentDate = new Date().toLocaleDateString();
+
   return (
-    <div>
-      <p>empty</p>
-    </div>
+    <TimerSection>
+      <h2>Hallo Max Mustermann, heute ist der {currentDate} !</h2>
+      <button>Start</button>
+      <button>Pause</button>
+      <h2>Counter:</h2>
+      <button>finish day</button>
+    </TimerSection>
   );
 };
 
