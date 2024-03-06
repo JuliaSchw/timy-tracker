12acffc2f2d257311513545816d2857eb2bad8a7
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-03-06 21:37:26 +0100

    update tsconfig to use ts-node command, add first data to dataset

--- index.ts
+++ index.ts
@@ -1,17 +1,32 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      role: "Nutzer",
+      email: "t.nauerz@uronovis.de",
+      surname: "Tobias",
+      lastname: "Nauerz",
+      department: "Innendienst",
+      position: "Customer-Service",
+      contract: 38.5,
+      birthday: new Date("2001-01-01"),
+      password: "uronovis",
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({});
+  console.dir(allUsers, { depth: null });
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
