1f5677b646a757a846e9ca971b9b048e39deed40
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2024-03-06 20:40:34 +0100

    update schema, initialize migration, install prisma client

---
+++ prisma/migrations/20240306193913_init_user_timer_work_summary_sick_vacation/migration.sql
@@ -0,0 +1,85 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "role" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "surname" TEXT,
+    "lastname" TEXT,
+    "department" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "contract" INTEGER NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Timer" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Timer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TimeInterval" (
+    "id" SERIAL NOT NULL,
+    "timerId" INTEGER NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3),
+
+    CONSTRAINT "TimeInterval_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkSummary" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "totalTimeWorked" INTEGER NOT NULL,
+
+    CONSTRAINT "WorkSummary_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SickDay" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SickDay_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VacationDay" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "approved" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "VacationDay_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_password_key" ON "User"("password");
+
+-- AddForeignKey
+ALTER TABLE "Timer" ADD CONSTRAINT "Timer_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TimeInterval" ADD CONSTRAINT "TimeInterval_timerId_fkey" FOREIGN KEY ("timerId") REFERENCES "Timer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkSummary" ADD CONSTRAINT "WorkSummary_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SickDay" ADD CONSTRAINT "SickDay_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VacationDay" ADD CONSTRAINT "VacationDay_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
